132bc893b891e41e5393e5d4eccf9c1e56d5bdc5
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-09-23 08:04:54 +0200

    good

---
+++ scripts/getdates.js
@@ -0,0 +1,28 @@
+const options = { weekday: "long", day: "numeric", month: "long", year: "numeric" };
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+
+const date = new Date();
+const year = date.getFullYear();
+const currentYear = `${year}`;
+document.querySelector("#year").textContent = currentYear;
+
+// Function to fetch weather data from OpenWeatherMap API and display it
+function displayWeather() {
+  const apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+  const city = "Harare";
+  const country = "ZW";
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const weatherDescription = data.weather[0].description;
+      const weatherElement = document.getElementById("weather");
+      weatherElement.textContent = `Weather: ${weatherDescription}`;
+    })
+    .catch(error => {
+      console.error("Error fetching weather data:", error);
+    });
+}
+
+// Call the function to display the current weather
+displayWeather();
