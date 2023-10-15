1ba5e9d94a108b5e6f1f143d096a0418b3d57b83
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2023-10-15 17:41:20 +0200

    new

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,21 @@ let currentYear = `${year}`;
 document.querySelector("#year").textContent = currentYear;
 
 document.getElementById("currentdate").textContent = document.lastModified;
+
+// Hamburger Javascript
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+function initMap() {
+	const location = { lat: -25.363, lng: 131.044 };
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: location
+	});
+}
